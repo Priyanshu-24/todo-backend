0f8afe06e99589e0cb9a63da0618031cc6ab6560
Priyanshu Pandey <priyanshu.pandey@jalantechnologies.com>
2022-01-18 15:10:17 +0530

    fixed 404 error

--- test/spec/todo/todo.spec.ts
+++ test/spec/todo/todo.spec.ts
@@ -75,12 +75,15 @@ describe('GET /todos/:id', () => {
     expect(res).to.have.status(200);
     expect(res.body).to.have.property('id');
     expect(res.body).to.have.property('title');
+
+    //both the IDs must be equal
+    expect(response.body.id).to.equal(res.body.id);
   });
 
   it('item is not present in the database', async () => {
     const id = '420';
     const res = await chai.request(expressApp).get(`/todos/${id}`);
 
-    expect(res).to.have.status(400);
+    expect(res).to.have.status(404);
   });
 });
